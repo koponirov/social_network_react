7a28d20443af8ef303857f40f863fd3cc0e3c0f6
koponirov <54912625+koponirov@users.noreply.github.com>
2019-10-02 23:14:09 +0300

    added 'src' folder

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://image.flaticon.com/icons/png/512/68/68637.png' alt='logo'></img>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://avatars.mds.yandex.net/get-pdb/1385901/97a55a4e-221b-436f-a746-bd680871dc5b/s1200' alt='space'></img>
+        </div>
+        <div>
+            ava+description
+        </div>
+        <div>
+            My posts
+        </div>
+        <div>
+            New post
+        </div>
+        <div>
+        <div>
+            post1
+        </div>
+        <div>
+            post2
+        </div>
+        </div>
+    </div>
+}
+
+export default Profile;
