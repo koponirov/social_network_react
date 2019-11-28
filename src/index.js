9aa95aa5810430d762b1f1de125c0d19abf2e228
m.koponirov <koponirov92@gmail.com>
2019-11-28 04:15:37 +0300

    FATAL ERROR: Objects are not valid as a React child (found: object with keys {$$typeof, type, compare, WrappedComponent, displayName}).  If you meant to render a collection of children, use an array instead.

--- src/index.js
+++ src/index.js
@@ -4,16 +4,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
 import {BrowserRouter} from "react-router-dom";
-import StoreContext from './StoreContext'
+import {Provider} from "react-redux";
+
 
 
 export let rerenderEntireTree = () => {
 
     ReactDOM.render(
         <BrowserRouter>
-            <StoreContext.Provider value={store}>
+            <Provider store={store}>
         <App />
-            </StoreContext.Provider>
+            </Provider>
         </BrowserRouter>, document.getElementById('root'));
 
 };
