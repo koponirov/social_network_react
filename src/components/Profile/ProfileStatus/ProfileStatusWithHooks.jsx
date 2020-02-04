3d4481edfc0dd8cd1ab7bbbd15ac43168ebc5b12
m.koponirov <koponirov92@gmail.com>
2020-02-04 22:04:06 +0300

    Used HOOK 'useEffect' in ProfileStatusWithHooks component, to display correct status.

--- src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,11 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const ProfileStatusWithHooks = (props) => {
 
     const [editMode,setEditMode] = useState(false);
     const [status,setStatus] = useState(props.status);
 
-
+    useEffect(()=>{
+        setStatus(props.status)
+    },[props.status])
     const activateEditMode = () => {
         setEditMode(true)
     };
