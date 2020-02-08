c06720525453fd39fd92a4704a31eb216ed4cdc3
m.koponirov <koponirov92@gmail.com>
2020-02-08 20:05:31 +0300

    Updates

---
+++ precache-manifest.2618b0676a446e4d6f99c7e8c9e04e03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5095cb6c48a13e942a0fae47d086a6e7",
+    "url": "/social_network_react/index.html"
+  },
+  {
+    "revision": "7b1a19d215361e455a6b",
+    "url": "/social_network_react/static/css/main.c532def9.chunk.css"
+  },
+  {
+    "revision": "8296ac6ec0309b83e76f",
+    "url": "/social_network_react/static/js/2.98db6a3f.chunk.js"
+  },
+  {
+    "revision": "7b1a19d215361e455a6b",
+    "url": "/social_network_react/static/js/main.75cf0b1f.chunk.js"
+  },
+  {
+    "revision": "b2d2d3f4879c31f4536f",
+    "url": "/social_network_react/static/js/runtime-main.37d20f1e.js"
+  },
+  {
+    "revision": "06b49fa87bb9ba5bbc954dc31667d2d4",
+    "url": "/social_network_react/static/media/loader.06b49fa8.svg"
+  }
+]);
