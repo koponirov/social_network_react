a7b06828d184407795a9c29a28028c601e18a9f9
m.koponirov <koponirov92@gmail.com>
2020-03-26 01:07:06 +0300

    deployed app to github pages

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social_network_react/precache-manifest.27ddd1a15c7b84b48568306064ba8f0a.js"
+  "/social_network_react/precache-manifest.d26bb2522b00126e6965b4f755669376.js"
 );
 
 self.addEventListener('message', (event) => {
